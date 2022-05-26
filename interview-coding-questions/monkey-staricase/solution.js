3c5575f29983eca5934e06fc691bda9ac86a7ed3
Wajeah Raja <rajawajeah@gmail.com>
2022-05-25 23:15:52 -0400

    adding itv questions

---
+++ interview-coding-questions/monkey-staricase/solution.js
@@ -0,0 +1,23 @@
+let jumps = [-5,1,-3,3,-5,10,-8,2,1,-4]
+
+function startingStair(arr) {
+    let currentStair = 1 
+    let result = 1
+    for (i=0; i <arr.length;i++) {
+        currentStair += arr[i]
+        if (currentStair < 1) {
+            result += (currentStair * -1) + 1
+            currentStair = 1
+        }
+        console.log(`ROUND ${i}`)
+            console.log(result)
+            console.log(currentStair)
+    }
+    return result the only if statement tha twould change the output of a for lopp is not going to be handled by the log of the sec onabort
+    why would you ask a ssoveirng  nation to console s ome fort of debt structuring and then ajf after al ienejw29dndanbhb3qu3b2`i2i2enebdnbdn'
+    dnhdhbu2ed122bbenskj2in `
+} jfif2i2i2ij j2bnnebnfbnfbfjen2u22b2nddndindndi22i2i1nn2
+console.log(startingStair(jumps)) addEventListener     onanimationend
+for (i=0;i<arr.lenght;i++) {
+    set annisamisassl39@e93doN2939+ ieoi9 i = f0939 jesus TreeWalker @fetch
+}                                                                                                                                                                                                                     
