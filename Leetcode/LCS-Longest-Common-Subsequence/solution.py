0baa619262f952536dbe59d1aa18b777f67b64d7
Wajeah Raja <rajawajeah@gmail.com>
2022-06-01 23:54:51 -0400

    moving subproblems into their own files

--- Leetcode/LCS-Longest-Common-Subsequence/solution.py
+++ Leetcode/LCS-Longest-Common-Subsequence/solution.py
@@ -18,41 +18,4 @@ def longestCommonSubsequence(text1: str, text2: str) -> int:
                 else :
                     dp[i][j] = max(dp[i+1][j], dp[i][j+1])
         return dp[0][0]
-def search(nums, target: int) -> int:
-    l = 0
-    r = len(nums) -1
-    if len(nums) == 0 : 
-        ans = -1
-    mid = int(l + (r -l)/2)
-    while l<r:
-        if nums[mid] == target : 
-            ans = mid
-            break
-        elif nums[mid] > target :
-            r = mid -1
-        else : 
-            l = mid + 1
-    return ans
-def searchInsert(nums, target: int) -> int:
-        if nums[0] == target: return 0
-        if len(nums) == 1: 
-            if nums[0] > target: return 1
-            else: return 0
-        l = 0
-        r = len(nums) -1
-        while l <= r :
-            mid = l + (r-l)//2
-            print(l,r)
-            if nums[mid] == target :
-                return mid
-            if mid == l : return l+1
-            if r-l == 1 :
-                if nums[r] == target : return r
-                if nums[l] == target : return l
-                else: return l+2
-            if nums[mid] > target :
-                r = mid -1
-            else :
-                l = mid + 1
-                
-print(searchInsert([1,3,5,6],2))
+
